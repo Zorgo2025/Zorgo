0c22713602d6d8eef5403515697f6c1a1afa165f
Zorgo2025 <allinweb@gmail.com>
2025-01-17 23:07:45 -0500

    Create deploy_token.sh

---
+++ deploy_token.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to deploy Zorgo token to Solana blockchain
+solana program deploy zorgo_token.sol
+
+echo "Token deployed successfully!"
+
+---
+
+### /scripts/interact_with_token.py
+import solana
+
+def check_balance(address):
+    # Function to check $ZORG balance
+    pass
+
+print("Interact with Zorgo token!")
+
+---

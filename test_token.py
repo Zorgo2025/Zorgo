e37b16156f95def758f7374d6f276ca89737c44c
Zorgo2025 <allinweb@gmail.com>
2025-01-17 23:09:19 -0500

    Create test_token.py

---
+++ test_token.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestZorgoToken(unittest.TestCase):
+    def test_transfer(self):
+        # Add test logic for token transfer
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
+---
